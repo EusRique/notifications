5fd666e2f4b6b14ef84dcdcef64ffe2239dc14fc
Henrique <hiquesc_@hotmail.com>
2018-09-11 22:25:30 -0300

    Notificação por email

--- app/Http/Controllers/Posts/CommentController.php
+++ app/Http/Controllers/Posts/CommentController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\Posts;
 use App\Http\Requests\StoreCommentFormRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use \App\Notifications\PostCommented;
 
 class CommentController extends Controller
 {
     public function store(StoreCommentFormRequest $request)
     {
         $comment = $request->user()->commets()->create($request->all());
+
+        $author = $comment->post->user;
+        $author->notify(new PostCommented($comment));
         
         return redirect()
                     ->route('posts.show', $comment->post_id)

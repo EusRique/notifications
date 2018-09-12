037995836884a6b0ce7331bcadd9a9b6eacbe9e8
Henrique <hiquesc_@hotmail.com>
2018-09-11 22:37:53 -0300

    notificações por Queues

--- app/Notifications/PostCommented.php
+++ app/Notifications/PostCommented.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class PostCommented extends Notification
+class PostCommented extends Notification implements ShouldQueue
 {
     use Queueable;
 
